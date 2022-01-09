459a71409c3e807d684401102a095969b156d10a
izumi0x01 <izumisoh@gmail.com>
2022-01-09 20:45:12 +0900

    最終バージョン

--- robot_sled/robot_sled.ino
+++ robot_sled/robot_sled.ino
@@ -67,11 +67,11 @@ front_leg1_servo_angles legRF1_angle  ={150, 120, 105, 90, 60, 15};
 front_leg2_servo_angles legLF2_angle  ={0, 45, 75, 135, 150};
 front_leg2_servo_angles legRF2_angle  ={0, 45, 75, 135, 150};
 
-front_leg3_servo_angles legLF3_angle  ={0, 7, 45, 90, 165};
-front_leg3_servo_angles legRF3_angle  ={0, 7, 45, 90, 165};
+front_leg3_servo_angles legLF3_angle  ={0, 20, 45, 75, 165};
+front_leg3_servo_angles legRF3_angle  ={0, 20, 45, 75, 165};
 
-back_leg_servo_angles legLB_angle  ={165, 100, 85, 10, 0};//default
-back_leg_servo_angles legRB_angle  ={0, 65, 80, 155, 165};
+back_leg_servo_angles legLB_angle  ={130, 100, 85, 10, 0};//default
+back_leg_servo_angles legRB_angle  ={35, 65, 80, 155, 165};
 
 
 void setup() {
@@ -90,38 +90,38 @@ void loop() {
 
 // 動作１
 
-delay(3000);
+delay(1000);
 pwm.setPWM(legLF3_pin, 0, map_angle(legLF3_angle.horizontal));
 pwm.setPWM(legRF3_pin, 0, map_angle(legRF3_angle.horizontal)); 
-delay(200);
+delay(100);
 pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.upper_limit));
 pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.upper_limit));  
 
 // 動作２
 
-delay(3000);
+delay(1000);
 pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.beyond_horizontal));
 pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.beyond_horizontal));  
-delay(200);
+delay(100);
 pwm.setPWM(legLF3_pin, 0, map_angle(legLF3_angle.lower_horizontal));
 pwm.setPWM(legRF3_pin, 0, map_angle(legRF3_angle.lower_horizontal)); 
 
 // 動作３
 
-delay(3000);
+delay(1000);
 pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.horizontal));
 pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.horizontal));  
-delay(500);
-//pwm.setPWM(legLB_pin, 0, map_angle(legLB_angle.backward_grounding));
-//pwm.setPWM(legRB_pin, 0, map_angle(legRB_angle.backward_grounding));
-delay(500);
+delay(100);
+pwm.setPWM(legLB_pin, 0, map_angle(legLB_angle.backward_grounding));
+pwm.setPWM(legRB_pin, 0, map_angle(legRB_angle.backward_grounding));
+delay(100);
 pwm.setPWM(legLF3_pin, 0, map_angle(legLF3_angle.diagonal));
 pwm.setPWM(legRF3_pin, 0, map_angle(legRF3_angle.diagonal)); 
 
 
 // 動作４
 
-delay(3000);
+delay(1000);
 pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.beyond_upward_vertical));
 pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.beyond_upward_vertical));  
 delay(500);
@@ -131,23 +131,23 @@ delay(500);
 pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.upward_vertical));
 pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.upward_vertical));   
 delay(500);
-pwm.setPWM(legLB_pin, 0, map_angle(legLB_angle.horizontal));
-pwm.setPWM(legRB_pin, 0, map_angle(legRB_angle.horizontal));
+//pwm.setPWM(legLB_pin, 0, map_angle(legLB_angle.horizontal));
+//pwm.setPWM(legRB_pin, 0, map_angle(legRB_angle.horizontal));
 
-delay(1000);
+//delay(1000);
 }
 
 void initialize_servo_degree(){
 
   while(true){
 
-    pwm.setPWM(legLF3_pin, 0, map_angle(legLF3_angle.horizontal));
-    pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.horizontal));
+    pwm.setPWM(legLF3_pin, 0, map_angle(legLF3_angle.downward_grounding));
+    pwm.setPWM(legLF2_pin, 0, map_angle(legLF2_angle.upward_vertical));
     pwm.setPWM(legLF1_pin, 0, map_angle(legLF1_angle.straight));
 
     //front right leg state
-    pwm.setPWM(legRF3_pin, 0, map_angle(legRF3_angle.horizontal));     
-    pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.horizontal));     
+    pwm.setPWM(legRF3_pin, 0, map_angle(legRF3_angle.downward_grounding));     
+    pwm.setPWM(legRF2_pin, 0, map_angle(legRF2_angle.upward_vertical));     
     pwm.setPWM(legRF1_pin, 0, map_angle(legRF1_angle.straight));
 
     //back left leg state
